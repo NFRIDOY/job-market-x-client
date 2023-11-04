6cb377fa39e6ff1ec3eba5889ec7a96e806c44da
NF RIDOY <nfridoy@gmail.com>
2023-11-05 03:35:38 +0600

    Feat: navbar avater fix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -25,21 +25,70 @@ export default function Navbar() {
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
+                        <li><a>Home</a></li>
                         <li tabIndex={0}>
                             <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
+                                <summary className="px-6">Manage Jobs</summary>
+                                <ul className="p-2 ">
+                                    <li className="w-fit"><a>Add Job</a></li>
+                                    <li className="w-fit"><a>My Posted Jobs</a></li>
+
                                 </ul>
                             </details>
                         </li>
-                        <li><a>Item 3</a></li>
+                        {/* <li><a>My Bids</a></li> */}
+
+                        <li tabIndex={0}>
+                            <details>
+                                <summary className="px-8">Job Bids</summary>
+                                <ul className="p-2">
+                                    <li><a>My Bids</a></li>
+                                    <li className="w-fit"><a>Bid Requests</a></li>
+                                </ul>
+                            </details>
+                        </li>
+                        <li><a>Registration</a></li>
+                        <li><a>Login</a></li>
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <a className="btn">Button</a>
+                    <div className="flex">
+                        {/* <div className="dropdown dropdown-end">
+                            <label tabIndex={0} className="btn btn-ghost btn-circle">
+                                <div className="indicator">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                                    <span className="badge badge-sm indicator-item">8</span>
+                                </div>
+                            </label>
+                            <div tabIndex={0} className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
+                                <div className="card-body">
+                                    <span className="font-bold text-lg">8 Items</span>
+                                    <span className="text-info">Subtotal: $999</span>
+                                    <div className="card-actions">
+                                        <button className="btn btn-primary btn-block">View cart</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div> */}
+                        <div className="dropdown dropdown-end">
+                            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                                <div className="w-10 mask mask-squircle">
+                                    <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                                </div>
+                            </label>
+                            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                                <li>
+                                    <a className="justify-between">
+                                        Profile
+                                        {/* <span className="badge">New</span> */}
+                                    </a>
+                                </li>
+                                <li><a>Settings</a></li>
+                                <li className="text-red-500 hover:text-red-700"><a>Logout</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    {/* <a className="btn">Button</a> */}
                 </div>
             </div>
         </div>
