be00830eb8536f8216efe8c8165d8a653ce6fbed
NF RIDOY <nfridoy@gmail.com>
2023-11-05 02:01:27 +0600

    Job Market X commit init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+// import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='bg-violet-800 btn btn-ghost'>Hi</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/MyBids/MyBids.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MyBids() {
+  return (
+    <div>MyBids</div>
+  )
+}
