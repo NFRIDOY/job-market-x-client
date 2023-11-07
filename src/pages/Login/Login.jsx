207b1cfffa5decb3f677c53c8858a112aed497e7
NF RIDOY <nfridoy@gmail.com>
2023-11-08 05:25:44 +0600

    Fix: google Icon set

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
 
 
 export default function Login() {
 
     const { user, setUser, createUserEmailPass, signInEmailPass, googleSignIn } = useAuth()
+    const navigate = useNavigate()
 
     const handleLogin = (e) => {
         e.preventDefault();
@@ -22,6 +23,9 @@ export default function Login() {
                 setUser(user)
                 alert("User Login")
                 console.log(user)
+                // console.log(location.pathname)
+                console.log(location?.state)
+                navigate(location?.state ? location?.state : '/')
             })
             .catch((error) => {
                 const errorCode = error.code;
@@ -45,6 +49,9 @@ export default function Login() {
                 alert("User Login Using Google")
                 setUser(user)
                 console.log(user)
+                // console.log(location.pathname)
+                console.log(location?.state)
+                navigate(location?.state ? location?.state : '/')
             }).catch((error) => {
                 // Handle Errors here.
                 const errorCode = error.code;
@@ -93,12 +100,12 @@ export default function Login() {
                                     {/* <a href="#" className="text-sm font-medium text-primary hover:underline dark:text-primary">Forgot password?</a> */}
                                 </div>
                                 <button type="submit" className="w-full text-white bg-primary hover:bg-primary focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary dark:hover:bg-primary dark:focus:ring-primary-800">Sign in</button>
-                                <button type="button" className="w-full  border-green-500 border-2 text-black hover:bg-white focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary dark:hover:bg-primary dark:focus:ring-primary-800 flex justify-between" onClick={hangleGoogleSignIn}>
+                                <button type="button" className="w-full  border-green-500 border-2 text-black hover:bg-white focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary dark:hover:bg-primary dark:focus:ring-primary-800 flex justify-between items-center" onClick={hangleGoogleSignIn}>
                                     <h1>
                                         Google
                                     </h1>
                                     <div>
-                                        I
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 488 512"><path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" /></svg>
                                     </div>
                                 </button>
                                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
