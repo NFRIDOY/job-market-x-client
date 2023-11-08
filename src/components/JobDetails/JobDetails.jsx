8b03b33a6bff0a1c872d84ad1d4a6b9718528b84
NF RIDOY <nfridoy@gmail.com>
2023-11-09 03:09:31 +0600

    Fix: some btn bug

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -340,8 +340,6 @@ export default function JobDetails() {
                                 name="submit"
                                 id="submit"
                                 className="w-full text-white bg-primary hover:bg-primary focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary dark:hover:bg-primary dark:focus:ring-primary-800 uppercase"
-                                placeholder="Job Title"
-
                             >
                                 Bid on the project
                             </button>
