74404445c65f781a532bb3a559ba0482576aeef8
NF RIDOY <nfridoy@gmail.com>
2023-11-09 18:55:29 +0600

    Fix: Reg UI

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -75,11 +75,11 @@ export default function Registration() {
               <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                 Registration for your account
               </h1>
-              <form className="space-y-4 md:space-y-6" onSubmit={handleReg}>
+              <form className="space-y-4 md:space-y-8 h-80" onSubmit={handleReg}>
 
                 <div className="flex space-x-4">
-                  <div>
-                    <div>
+                  <div className="space-y-8">
+                    <div >
                       <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                       <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@mail.com" required />
                     </div>
@@ -88,7 +88,7 @@ export default function Registration() {
                       <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                     </div>
                   </div>
-                  <div>
+                  <div className="space-y-8">
                     <div>
                       <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
                       <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name" required />
